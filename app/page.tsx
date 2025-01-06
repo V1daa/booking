09c2ef112dd721b665beb8e14abde67ff9b80b68
V1daa <mrjokerekoj@gmail.com>
2025-01-06 18:46:13 +0100

    Fix1

--- app/page.tsx
+++ app/page.tsx
@@ -172,7 +172,7 @@ export default function Home({
                       defaultMonth={date?.from}
                       selected={date}
                       onSelect={setDate}
-                      numberOfMonths={2}
+                      numberOfMonths={1}
                     />
                   </PopoverContent>
                 </Popover>
